d63ca3eb0158ffd0ab34b6ba9987af140f014bf2
blawesom <benjamin.laplane@outscale.com>
2018-01-30 11:05:29 +0100

    Proper arg parsing for shell script

--- setup_vpn.sh
+++ setup_vpn.sh
@@ -1,9 +1,23 @@
 #/bin/bash
 
+# Use the following options: -x pathto/vpn.xml -k pathto/key.rsa
+
+# Get arguments
+xml=''
+key=''
+
+while getopts 'x:k:' flag; do
+  case "${flag}" in
+    x) xml="${OPTARG}" ;;
+    k) key="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+# Clean previous deployments
 rm -rf ./roles/vpn/vars/main.yml
 rm -rf inventory
 
-# ingest vpn conf xml file
-python xmltovars.py /Users/benjaminlaplane/Downloads/vpn-e04b7aca.xml
-# change path to ssh key
-ansible-playbook -i inventory vpn.yml --key-file "~/.ssh/outscale_demo.rsa"
+# Generate vars file and play book
+python xmltovars.py $xml
+ansible-playbook -i inventory vpn.yml --key-file $key
